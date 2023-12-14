7915a0a1557ccbec0168e0c084887961ccf5b3dd
Sooliks <evgenij.terexov.2000@mail.ru>
2023-12-15 00:39:57 +0300

    fix prod

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  important: true
+}
+
